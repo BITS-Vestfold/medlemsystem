7aeb1b28b14cb26775b993a952437e09233293e3
jeanray <jean.oedegaard@gmail.com>
2017-10-18 00:01:57 +0200

    Skallfil-klasse for endre/slette medlem

---
+++ klasser/medlemdata.php
@@ -0,0 +1,10 @@
+<?php
+  // Funksjonalitet for å søke opp og endre på informasjon om et medlem
+  // Kan sannsynlig bruke funksjoner fra klassen før søk - må ha mulighet for å velge ut medlem
+
+  // Lister så all informasjon om valgte medlem i et HTML-skjema skrevet ut av PHP,
+  // med eksisterende verdier satt i de tilhørende feltene. Det aktuelle kan så endres,
+  // for å så lagres til databasen.
+
+  // Samme skjema kan inneholde en knapp for sletting av medlem. Man får da opp bekreftelsesspørsmål.
+?>
