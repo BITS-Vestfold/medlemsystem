366550f8d496314ed6be5d1a9285d735b7f336ed
jeanray <jean.oedegaard@gmail.com>
2017-10-17 23:45:18 +0200

    HTML-del, basic brukerregistrering
    Trenger mer beskyttelse mot boter ol. på sikt (recaptcha osv.)

--- innlogging/registrer_ny_bruker.php
+++ innlogging/registrer_ny_bruker.php
@@ -1,3 +1,21 @@
 <?php
+  <form id="innlogging" method="POST">
+  <div class="inndatafelt">
+    <label for="brukernavn"</label>
+    <input type="text" name="brukernavn" id="brukernavn" autocomplete="off">
+  </div>
 
+  <div class="inndatafelt">
+    <label for="passordEn"></label>
+    <input type="password" name="passordEn" id="passordEn">
+  </div>
+
+  <div class="inndatafelt">
+    <label for="passordTo"></label>
+    <input type="password" name="passordTo" id="passordTo">
+  </div>
+
+  <input type="submit" value="Registrer bruker">
+
+  </form>
 ?>
