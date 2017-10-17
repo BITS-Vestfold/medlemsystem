04d211421d5a9ff92ce74b4e1ba66c50dd28f3fd
jeanray <jean.oedegaard@gmail.com>
2017-10-18 00:02:11 +0200

    Skallfil - klasse for søkefunksjoner

---
+++ klasser/sok.php
@@ -0,0 +1,17 @@
+<?php
+
+// Søkefunksjon som mulig må deles opp i mindre funksjoner for å få det opp og gå.
+// Ved søk burde man kunne få opp for eksempel lister over de som ikke har betalt kontigent,
+// og i bunn og grunn kunne søke på alle kriterer. (Kun for admins)
+
+// Medlemmer man søker opp bør på sikt kunne velges for å så bli sendt videre via en superglobal
+// til for eks. endring av medlem. (radioboks, et medlem av gangen enn så lenge)
+
+function listAlleMedlemmer() {
+  // Utføre SQL-spørring
+  // Iterere array med svar, wrappe de inn i en HTML-tabell
+  // På sikt bør utskrevne svar begrenses til for eks 25 per side,
+  // og støtte for flere sider lages.
+}
+
+?>
