69bfdd167a0310a00c1d65758a1c28f01151e977
jeanray <jean.oedegaard@gmail.com>
2017-10-18 15:42:31 +0200

    Små feil-endringer for navbaren

--- funksjoner/print_navbar.php
+++ funksjoner/print_navbar.php
@@ -11,14 +11,14 @@ echo <<<'navbarHTML'
     <div class="container-fluid">
       <div class="navbar-header">
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1-collapse" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span> <!-- Alle disse span-taggene, er de egentlig i bruk?-->
+          <span class="sr-only">Toggle navigation</span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
         <a class="navbar-brand" href="index.php">BITS - medlemsregister</a>
       </div>
-      <!-- Collect the nav links, forms, and other content for toggling -->
+
       <div class="collapse navbar-collapse" id="navbar1-collapse">
         <ul class="nav navbar-nav">
 
@@ -37,7 +37,7 @@ echo <<<'navbarHTML'
           </li>
 
           <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Vis medlemsliste<span class="caret"></span></a>
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Medlemsliste <span class="caret"></span></a>
             <ul class="dropdown-menu">
 
               <li><a href="?funksjon=vis_alle_medlemmer">Alle medlemmer</a></li>
--- index.php
+++ index.php
@@ -21,6 +21,9 @@ include 'funksjoner/print_navbar.php';
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 
+  <!-- JS-scripts skal egentlig bli lagt inn til slutt for å sikre raskest mulig sidelasting... -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
   <link href="css/jquery-ui.css" rel="stylesheet">
   <link href="css/jquery-ui.theme.css" rel="stylesheet">
 
