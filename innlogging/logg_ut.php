f579a8c42b99a8420d790ccd7d6c62affd22cdb7
jeanray <jean.oedegaard@gmail.com>
2017-10-26 00:56:09 +0200

    Lagt til tekst ved utlogging

--- innlogging/logg_ut.php
+++ innlogging/logg_ut.php
@@ -1,5 +1,5 @@
 <?php
-
 session_destroy();
-
+print("<div class=\"alert alert-info\">Du er nå logget ut av systemet.<br>">
+print("<a href=\"../index.php\">Gå tilbake til forsiden.</a></div>");
 ?>
