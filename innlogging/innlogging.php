4908f84a012fe4932c5322974aa22c34e00188b5
jeanray <jean.oedegaard@gmail.com>
2017-10-26 00:51:51 +0200

    Endret innloggingssystem noe

--- innlogging/innlogging.php
+++ innlogging/innlogging.php
@@ -1,8 +1,16 @@
-<?php
-?>
 <!DOCTYPE html>
 <html lang="no">
 <head>
+  <!-- Støtte for favicons på forskjellige systemer/browsere-->
+  <link rel="apple-touch-icon" sizes="180x180" href="../favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="../favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="../favicons/favicon-16x16.png">
+  <link rel="manifest" href="../favicons/manifest.json">
+  <link rel="mask-icon" href="../favicons/safari-pinned-tab.svg" color="#5bbad5">
+  <link rel="shortcut icon" href="../favicons/favicon.ico">
+  <meta name="msapplication-config" content="../favicons/browserconfig.xml">
+  <meta name="theme-color" content="#ffffff">
+
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,7 +18,7 @@
   <title>Innlogging - BITS</title>
   <!-- CSS-filer -->
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/custom.css" rel="stylesheet">
+  <link href="../css/login.css" rel="stylesheet">
 
   <!-- jQuery må være med av diverse grunner (BS++) -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -18,7 +26,6 @@
 
   <!-- JS-scripts skal egentlig bli lagt inn til slutt for å sikre raskest mulig sidelasting... -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script src="js/confirm.js"></script>
 
 </head>
 <body>
@@ -33,7 +40,7 @@
             <div class="alert alert-info" role="alert">
               <b>Sidene du forsøker å nå krever en innlogget bruker. Vennligst logg inn.</b>
             </div>
-          <form id="loginSkjema" name="loginSkjema" method="POST" action="logginn.php">
+          <form id="loginSkjema" name="loginSkjema" method="POST">
             <!-- attributter med default verdier er utelatt-->
             <input type="text" id="brukernavn" name="brukernavn" class="form-control input-group" placeholder="Brukernavn">
             </br>
@@ -56,3 +63,19 @@
 </div>
 </body>
 </html>
+<?php
+if (isset($_POST["sendLogin"])) {
+  include_once('sys-funksjoner.php');
+  include_once('../include/dbtilkobling.php'); // Gir oss tilgang til MySQL-databasen via koblingsobjektet $dbLink (MySQLi)
+
+  session_start();
+
+  $brukerFraSkjema = htmlspecialchars($dbLink->real_escape_string($_POST["brukernavn"]));
+  $passordFraSkjema = htmlspecialchars($dbLink->real_escape_string($_POST["passord"]));
+
+  trim($brukerFraSkjema);
+  trim($passordFraSkjema);
+
+  sjekkLogin($brukerFraSkjema, $passordFraSkjema, $dbLink);
+}
+?>
