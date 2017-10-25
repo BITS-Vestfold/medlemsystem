e7b32d084a44c475231d7b2b036cea54272758b4
jeanray <jean.oedegaard@gmail.com>
2017-10-26 00:55:49 +0200

    Endringer i innloggingssystemet

--- innlogging/sys-funksjoner.php
+++ innlogging/sys-funksjoner.php
@@ -1,24 +1,35 @@
 <?php
 // Fil som tar imot data fra skjemaet for innlogging
-
+session_start();
 
 function sjekkLogin($brukernavn, $passord, $dbConn) {
   // Foreløpig kun tilpasset adminbrukere
-  print("Forsøker...<br>");
   $sql = "SELECT passord FROM admin WHERE adminbrukernavn='$brukernavn';";
   // Utfør spørring
-  $sqlSvar = $dbConn->query($sql);
+  $sqlSvar = $dbConn->query($sql) OR die("Noe gikk feil.");
   $dbPassord = $sqlSvar->fetch_assoc();
-  var_dump($brukernavn);
-  print("<br>");
-  var_dump($passord);
-  print("<br>");
-  var_dump($dbPassord);
-  print("<br>");
-  if (password_verify($passord, $dbPassord)) {
-    print("Yes");
+
+  if (password_verify($passord, $dbPassord["passord"])) {
+    print("<div class=\"alert alert-success\">Innlogging vellykket. <a href=\"../index.php\">Du blir nå vidersendt.</a></div>");
+    //$mld["LoginOk"]
+    echo('<META HTTP-EQUIV=REFRESH CONTENT="2;../index.php">');
+
+    // Hvis innloggingen er vellykket, altså at passord_verify returnerer true,
+    // setter vi $_SESSION-variabler som gjenspeiler dette.
+    $_SESSION["brnavn"] = "$brukernavn";
+    $_SESSION["loggetinn"] = "1";
+
+    // Midlertidig blir sikkerhetenssjekken kun basert på session-variabler.
+    // Det bør og kan legges på flere lag med sikring.
+
+    //  $logginnTid = date("Y-m-d H:i:s", time());
+    //  Fremtidig: logg innloggingen til databasen
+
   } else {
-    print("ooops.");
+    print("<div class=\"alert alert-danger\">Dette gikk galt. Vennligst forsøk på ny.</div>");
+
+    // Fremtidig funksjon bør se på brukernavn, og legge til funksjonalitet
+    // for midlertidig utestengelse om n-antall mislykkede forsøk blir utført.
   }
 }
 
