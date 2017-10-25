316f4ea09b752e4cdc6e1f21ad5b35ddb32d51cc
jeanray <jean.oedegaard@gmail.com>
2017-10-26 00:51:23 +0200

    faviconstuff

--- index.php
+++ index.php
@@ -10,11 +10,21 @@ include_once('funksjoner/print_navbar.php');
 <!DOCTYPE html>
 <html lang="no">
 <head>
+  <!-- Støtte for favicons på forskjellige systemer/browsere-->
+  <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+  <link rel="manifest" href="/favicons/manifest.json">
+  <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5">
+  <link rel="shortcut icon" href="/favicons/favicon.ico">
+  <meta name="msapplication-config" content="/favicons/browserconfig.xml">
+  <meta name="theme-color" content="#ffffff">
+
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<title>BITS - Medlemsregistrering</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title>BITS - Medlemsregistrering</title>
   <!-- CSS-filer (bruker CDN-utgave av bootstrap midlertidig, frontend-team gjør om ev.) -->
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
   <link href="css/custom.css" rel="stylesheet">
