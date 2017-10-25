e1250ad4e3d8368ce870ee007aed991ada86c547
jeanray <jean.oedegaard@gmail.com>
2017-10-26 01:20:01 +0200

    Forenklet forside uten innlogging

---
+++ forside.php
@@ -0,0 +1,5 @@
+<?php
+print("<h2>BITS medlemssystem</h2>\n");
+print("<h4>Velkommen hit.</h4>\n");
+print("Her blir det i stedet inkludering av en fil med nowdow-funksjoner for å skrive nødvendig HTML for forsiden.<br>Dette i håp om å forenkle jobben med å style siden.<br><br>\n");
+?>
