6686e11460b2dcd932a7f9de53c68f323b8608fd
jeanray <jean.oedegaard@gmail.com>
2017-10-26 03:23:09 +0200

    visning av skjema for medlemreg

--- medlemsregistrering.php
+++ medlemsregistrering.php
@@ -1,4 +1,6 @@
 <?php
+include("funksjoner/print_skjema.php");
+print("<h3>OBS: IKKE FERDIGLAGET; KUN OPPE FOR TESTING</h3>");
 print("<h4>Medlemsregistrering</h4>");
-// inkluder filer for tegning av registreringsskjema
+skrivRegMedlemSkjema();
 ?>
